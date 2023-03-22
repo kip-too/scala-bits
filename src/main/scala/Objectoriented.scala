393f70b20651cfc7830ecda159f45af2389fbcf7
kip-too <hoseakiptoo201@gmail.com>
2023-03-22 14:44:06 +0300

    ooop day 1

---
+++ src/main/scala/Objectoriented.scala
@@ -0,0 +1,9 @@
+object Objectoriented extends  App{
+  class Animal{
+    
+  }
+  val animal = new Animal
+  class Dog extends Animal{
+
+  }
+}
---
+++ src/main/scala/Robot.scala
@@ -0,0 +1,5 @@
+class Robot  (name:String){
+  val tom = new Robot("Tom")
+  val john = new Robot("John")
+  println(tom)
+}
